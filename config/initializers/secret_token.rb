c00ce50a18bb54d1ab3bc95174c83ca8e501d123
cortex <cortex@corp.(none)>
2011-06-25 13:55:53 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '9d1611391f21994b8fc58d256a304ff417a9f5ba3410ddc13aa764475fd3e9d447b58e5be08c7fd2223fd10b08e2df140b5dfad463bd1d9e764149abf87d85ff'
